afac8447d395716303b187bc55f3a97909b83956
seonyeonghun <sprax@naver.com>
2023-09-22 15:08:52 +0900

    TodoInput created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
-import './App.css';
-
+import "./App.css";
+const TodoInput = (props) => {
+  return <div>hi</div>;
+};
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <TodoInput />
     </div>
   );
 }
